8d549f5004b2302052d3f3f834ab45b58d975034
Sheel Patel <hello@sheel.me>
2014-08-02 18:32:00 -0600

    Initial Commit

---
+++ duosexagesimal-system.js
@@ -0,0 +1,57 @@
+var digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split('');
+
+/**
+ *
+ * @param {string} duosexagesimalNumber
+ * @returns {number} decimalNumber
+ */
+module.exports.toDecimal = function toDecimal(duosexagesimalNumber) {
+
+  /* Enforce prerequisites */
+
+  if (typeof duosexagesimalNumber !== 'string') return;
+
+  /* Convert the number from base 62 to base 10 */
+
+  var decimalNumber = 0;
+
+  for (var i = 0; i < duosexagesimalNumber.length; i++) {
+    var exponent = duosexagesimalNumber.length - i - 1;
+    var index = digits.indexOf(duosexagesimalNumber[i]);
+
+    if (index === -1) return null;
+
+    decimalNumber += (index) * Math.pow(digits.length, exponent);
+  }
+
+  /* Return null if decimal number is so large that precision is lost */
+
+  if (decimalNumber >= 9007199254740992) {
+    return null;
+  }
+
+  return decimalNumber;
+};
+
+/**
+ *
+ * @param {number} decimalNumber
+ * @returns {string} duosexagesimalNumber
+ */
+module.exports.fromDecimal = function fromDecimal(decimalNumber) {
+
+  /* Enforce prerequisites */
+
+  if (decimalNumber !== (decimalNumber | 0)) return null;
+
+  /* Convert the number from base 10 to base 62 */
+
+  var buffer = [];
+
+  do {
+    buffer.push(digits[Math.round(decimalNumber % digits.length)]);
+    decimalNumber = Math.floor(decimalNumber / digits.length);
+  } while (decimalNumber >= 1);
+
+  return buffer.reverse().join('');
+};
---
+++ test.js
@@ -0,0 +1,55 @@
+var duosexagesimal = require('./duosexagesimal-system');
+
+var assert = require("assert")
+
+describe('duosexagesimal', function () {
+
+  describe('#toDecimal()', function () {
+    it('should return null if no arguments passed', function () {
+      assert.equal(duosexagesimal.toDecimal(), null);
+    });
+    it('should return null if argument is not a string', function () {
+      assert.equal(duosexagesimal.toDecimal({}), null);
+    });
+    it('should return null if null is passed', function () {
+      assert.equal(duosexagesimal.toDecimal(null), null);
+    });
+    it('should return null if argument is a string but not alphanumeric', function () {
+      assert.equal(duosexagesimal.toDecimal('a!'), null);
+    });
+    it('should return null when the decimal value will be too large', function () {
+      assert.equal(duosexagesimal.toDecimal('fFgnDxSe8'), null);
+    });
+    it('should return correct decimal values', function () {
+      assert.equal(duosexagesimal.toDecimal('0'), 0);
+      assert.equal(duosexagesimal.toDecimal('5'), 5);
+      assert.equal(duosexagesimal.toDecimal('A'), 10);
+      assert.equal(duosexagesimal.toDecimal('a'), 36);
+      assert.equal(duosexagesimal.toDecimal('10'), 62);
+      assert.equal(duosexagesimal.toDecimal('1c'), 100);
+    });
+  });
+
+  describe('#fromDecimal()', function () {
+    it('should return null if no arguments passed', function () {
+      assert.equal(duosexagesimal.fromDecimal(Math.pow(2, 53)), null);
+    })
+    it('should return null if argument is not a number', function () {
+      assert.equal(duosexagesimal.fromDecimal({}), null);
+    })
+    it('should return null if argument is not a whole number', function () {
+      assert.equal(duosexagesimal.fromDecimal(3.14), null);
+    });
+    it('should return null if null is passed', function () {
+      assert.equal(duosexagesimal.fromDecimal(null), null);
+    });
+    it('should return correct duosexagesimal values', function () {
+      assert.equal(duosexagesimal.fromDecimal(0), '0');
+      assert.equal(duosexagesimal.fromDecimal(5), '5');
+      assert.equal(duosexagesimal.fromDecimal(10), 'A');
+      assert.equal(duosexagesimal.fromDecimal(36), 'a');
+      assert.equal(duosexagesimal.fromDecimal(62), '10');
+      assert.equal(duosexagesimal.fromDecimal(100), '1c');
+    });
+  });
+})
